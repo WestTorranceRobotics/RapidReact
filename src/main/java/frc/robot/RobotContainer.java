824b1d40eee583102a1da9b3ef29ef05a0c71390
SrilakshmiC <1017491@etusd.org>
2022-01-09 14:58:42 -0800

    Fixed whitespace

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -45,7 +45,6 @@ public class RobotContainer {
   
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-
     // Configure the button bindings
     configureSubsystems();
     configureDefaultCommands();
