1b52de48597c375a960aec594e9184b922da6416
Pineapple131 <92963579+Pineapple131@users.noreply.github.com>
2022-01-21 16:04:30 -0800

    StopShooter cmd REVIST AFTER SUBSYSTEM COMPLETE

---
+++ src/main/java/frc/robot/commands/StopShooter.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+//imports
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Shooter;
+
+
+public class StopShooter extends CommandBase {
+//making shooter for stopping
+private Shooter shooterStop;
+
+
+
+  /** Creates a new StopShooter. */
+  public StopShooter(Shooter shooter) {
+shooterStop = shooter;
+addRequirements(shooter);
+
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    //need to add direct power method in subsystem, when it is done add StopShooter.directPower(0); to execute
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
