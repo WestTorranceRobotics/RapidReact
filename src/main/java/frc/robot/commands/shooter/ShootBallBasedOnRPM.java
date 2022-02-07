bb7ec7e1ca4c8b60228e67662de0c42ccaec06ef
Pineapple131 <92963579+Pineapple131@users.noreply.github.com>
2022-02-07 15:19:24 -0800

    RPM shooter cmd w/o loader

--- src/main/java/frc/robot/commands/shooter/ShootBallBasedOnRPM.java
+++ src/main/java/frc/robot/commands/shooter/ShootBallBasedOnRPM.java
@@ -5,33 +5,64 @@
 package frc.robot.commands.shooter;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 
 public class ShootBallBasedOnRPM extends CommandBase {
+
   private Shooter bShooter;
+  private double rpm;
+ //private Loader loader;
+
   /** Creates a new ShootBallBasedOnRPM. */
-  public ShootBallBasedOnRPM(Shooter shooter) {
-bShooter = shooter;
-addRequirements(bShooter);
+
+  //add Loader loader in ()
+  public ShootBallBasedOnRPM(Shooter shooter, double rpm) {
+   
+    this.bShooter = shooter;
+    this.rpm = rpm;
+//private Loader loader;
+
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    //put rpm value in ()
-bShooter.startShooter();
+    
+    //loader.ballIntaked(0);
 
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    
+    double error = bShooter.getVelocity() - rpm;
+    double comp = -1.2e-5 * Math.pow(error,3);
+    if (Math.abs(comp) > 0.2) {
+      comp = Math.signum(comp) * 2;
+    }
+    bShooter.directVolts(0.147 + 0.0015538 * rpm );
+
+    /* if (shooter.getVelocity() >= rpm-50 && loader.getAppliedOutput() == 0) {
+      loader.runBelt(RobotMap.LoaderMap.runLoaderSpeed);
+      shooter.atSpeed(true); */
+
+      /* if (shooter.atSpeed()) {
+        shooter.currentWatch(RobotMap.ShooterMap.lineShootRPM);
+       }
+        */
+  }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    bShooter.stopShooter();
+
+    bShooter.directPower(0);
+    bShooter.atSpeed(false);
+   //loader.stopBelt();
+   
   }
 
   // Returns true when the command should end.
